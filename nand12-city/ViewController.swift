08a8356fbe1c0fd40e98cd820e1dad8a8379f034
Access-Lab <wsuaccesslab@gmail.com>
2022-12-20 16:10:26 -0600

    Initial Commit

---
+++ nand12-city/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nand12-city
+//
+//  Created by wh-308 on 12/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nand12-cityUITests/nand12_cityUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nand12_cityUITestsLaunchTests.swift
+//  nand12-cityUITests
+//
+//  Created by wh-308 on 12/20/22.
+//
+
+import XCTest
+
+final class nand12_cityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
